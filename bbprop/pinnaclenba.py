0c6fb7bb0d4a703baf0af42b56abef7c40eeef4f
Jeremy Morrison <jeremy.morrison36@gmail.com>
2021-02-06 15:42:05 -0800

    Add old files

--- bbprop/pinnaclenba.py
+++ bbprop/pinnaclenba.py
@@ -1,5 +1,5 @@
 import logging
-from typing import Optional, Dict
+from typing import Optional, Dict, List, Tuple
 
 from nba_api.stats.static import players
 
@@ -7,18 +7,74 @@ logger = logging.getLogger(__name__)
 
 
 class PinnacleNBA:
-    def player_from_description(self, desc: str) -> Optional[Dict]:
-        """From Pinnacle prop description, find a player's name and NBA ID."""
-        pl = desc.split()[:2]
-        p_name = " ".join(pl)
-        if not all(p.isalpha() for p in pl):
-            logger.warning(f'Player name is malformed: "{p_name}".')
-            return
+    def _parse_description(self, desc: str) -> Tuple[str]:
+        """Parse player description into name and scoring categories.
 
-        ps = players.find_players_by_full_name(p_name)
+        Returns:
+            Tuple of name and category string.
+        """
+        oi = desc.find("(")
+        ci = desc.find(")")
+        if oi == -1 or ci == -1:
+            logger.warning(f'Prop description is malformed: "{desc}".')
+            return "", ""
+
+        name = desc[:oi].strip()
+        cat = desc[oi + 1 : ci]
+        return name, cat
+
+    def player_from_description(self, desc: str) -> Dict:
+        """From Pinnacle prop description, find a player's name and NBA ID."""
+        name, _ = self._parse_description(desc)
+
+        if not name:
+            return {}
+
+        ps = players.find_players_by_full_name(name)
         if len(ps) == 0:
-            logger.warning(f'Could not find player "{p_name}".')
+            logger.warning(f'Could not find player "{name}".')
+            return {}
         elif len(ps) > 1:
-            logger.warning(f'Multiple player matches for "{p_name}".')
+            logger.warning(f'Multiple player matches for "{name}".')
+            return {}
         else:
             return ps[0]
+
+    def categories_from_description(self, desc: str) -> Tuple[str]:
+        """From Pinnacle prop description, find the prop's scoring categories, as named
+        in NBA API.
+
+        Returns:
+            Tuple of categories the prop includes.
+        """
+        translations = {
+            "points": ("pts"),
+            "assists": ("ast"),
+            "turnovers": ("tov"),
+            "3 point fg": ("fg3m"),
+            "rebounds": ("reb"),
+            "pts+rebs+asts": ("pts", "reb", "ast"),
+            "steals+blocks": ("stl", "blk"),
+            "double+double": ("dd2"),
+        }
+        _, cat = self._parse_description(desc)
+        cat = cat.lower()
+        try:
+            return translations[cat]
+        except KeyError:
+            logger.warning(f'Could not find scoring category "{cat}".')
+            return ()
+
+    def merge_props_and_players(self, props: Dict):
+        """Get NBA player information for each prop bet dict."""
+        merged = []
+        for id_, p in props.items():
+            desc = p["description"]
+            player = self.player_from_description(desc)
+            cats = self.categories_from_description(desc)
+            if not player or not cats:
+                continue
+
+            m = {
+                "name": player["full_name"],
+            }
--- tests/test_pinnacle_nba.py
+++ tests/test_pinnacle_nba.py
@@ -1,20 +1,34 @@
 import pytest
 
-from bbprop.pinnaclenba import PinnacleNBA
-
 
 @pytest.mark.parametrize(
     "desc, expected",
     [
         ("Isaac Okoro (Steals+Blocks)(must start)", "Isaac Okoro"),
-        ("Foo Bar (Steals+Blocks)(must start)", None),
-        ("James (Steals+Blocks)(must start)", None),
-        ("", None),
+        ("Foo Bar (Steals+Blocks)(must start)", {}),
+        ("James (Steals+Blocks)(must start)", {}),
+        ("", {}),
     ],
 )
-def test_player_from_description(desc, expected):
-    result = PinnacleNBA().player_from_description(desc)
-    if result is None:
+def test_player_from_description(desc, expected, pinnaclenba):
+    result = pinnaclenba.player_from_description(desc)
+    if not result:
         assert result == expected
     else:
         assert result["full_name"] == expected
+
+
+@pytest.mark.parametrize(
+    "desc, expected",
+    [
+        ("Isaac Okoro (Steals+Blocks)(must start)", ("stl", "blk")),
+        ("Isaac Okoro (points)(must start)", ("pts")),
+    ],
+)
+def test_categories_from_desscription(desc, expected, pinnaclenba):
+    assert pinnaclenba.categories_from_description(desc) == expected
+
+
+def test_merge_props_and_players(straight, related, testpinnacle, pinnaclenba):
+    props = testpinnacle.build_props(straight, related)
+    pinnaclenba.merge_props_and_players(props)
