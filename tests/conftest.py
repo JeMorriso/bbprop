11d1b05e49748f4f7ad5e23c88b7d0c9a1e18962
Jeremy Morrison <jeremy.morrison36@gmail.com>
2021-02-06 15:38:21 -0800

    Update tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,14 @@
 import json
+from pathlib import Path
 
 import pytest
+import pandas as pd
 from seleniumwire import webdriver
 
 from bbprop.pinnaclenba import PinnacleNBA
 from bbprop.pinnacle import Pinnacle, PinnacleGame
+from bbprop.sportapi import NBA
+from bbprop.betrange import Last10, Last3, Last5, Season
 
 
 class TestPinnacle(Pinnacle):
@@ -40,3 +44,44 @@ def related():
 @pytest.fixture
 def pinnaclegame(straight, related):
     return PinnacleGame(straight, related, False)
+
+
+@pytest.fixture
+def nba():
+    return NBA()
+
+
+# @pytest.fixture
+# def betranges():
+#     return BetRanges()
+
+
+@pytest.fixture
+def last3():
+    return Last3()
+
+
+@pytest.fixture
+def last5():
+    return Last5()
+
+
+@pytest.fixture
+def last10():
+    return Last10()
+
+
+@pytest.fixture
+def season():
+    return Season()
+
+
+@pytest.fixture
+def gamelogs():
+    dfs = {}
+    p = Path("tests/csv")
+    for fp in p.iterdir():
+        with open(fp, "r") as f:
+            p_name = " ".join(str(fp).split()[:2]).split("/")[-1]
+            dfs[p_name] = pd.read_csv(f)
+    return dfs
---
+++ tests/test_sanity.py
@@ -0,0 +1,58 @@
+import time
+
+from bbprop.betrange import BetRanges
+
+
+class TestPinnacle:
+    def test_game_id(testpinnacle):
+        id_ = 1249774441
+        url1 = "https://www.pinnacle.com/en/basketball/nba/detroit-pistons-vs-cleveland-cavaliers/1249774441"
+        url2 = "https://www.pinnacle.com/en/basketball/nba/detroit-pistons-vs-cleveland-cavaliers/1249774441/"
+
+        pin = testpinnacle
+        assert int(pin._game_id(url1)) == id_
+        assert int(pin._game_id(url2)) == id_
+
+    # Test requires NEW game link
+    # def test_game(testpinnacle):
+    #     data = testpinnacle.game(
+    #         url="https://www.pinnacle.com/en/basketball/nba/detroit-pistons-vs-cleveland-cavaliers/1249774441"
+    #     )
+    #     print(data)
+
+
+class TestPinnacleGame:
+    def test_prop_bets(pinnaclegame):
+        pinnaclegame.prop_bets()
+
+
+class TestNBA:
+    def test_player_by_name(self, pinnaclegame, nba):
+        for prop in pinnaclegame.prop_bets():
+            p = nba.player_by_name(prop.name)
+
+            print(p)
+            if p:
+                assert "full_name" in p
+            time.sleep(3)
+
+    def test_season_game_log(self, pinnaclegame, nba):
+        for prop in pinnaclegame.prop_bets():
+            ids = {}
+            if prop.name not in ids:
+                ids[prop.name] = nba.player_by_name(prop.name)
+                glg = nba.season_game_log(ids[prop.name]["id"], "2020-21")
+                glg.to_csv(f"tests/csv/{prop.name} 2020-21.csv")
+
+                time.sleep(3)
+
+
+class TestBetRanges:
+    def test_calc_values(self, pinnaclegame, gamelogs, last3, last5, last10, season):
+        bets = pinnaclegame.prop_bets()
+        bet_values = []
+        for b in bets:
+            br = BetRanges(b, gamelogs[b.name], last3, last5, last10, season)
+            br.calc_values()
+            bet_values.append(br)
+        assert True
