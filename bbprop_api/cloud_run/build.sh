c16c9d369b399788aff60096efb13dbe8a245369
Jeremy Morrison <jeremy.morrison36@gmail.com>
2021-02-16 14:26:53 -0800

    Add build shell script, modify Dockerfile
    Script makes it easier to build both locally and on Google Cloud.

---
+++ bbprop_api/cloud_run/build.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/sh
+
+################################################################################
+# If Dockerfile is not in working dir when gcloud builds command is run, it
+# causes problems. Simple solution is copy bbprop module into current directory,
+# and then delete it when the script is done.
+################################################################################
+
+ENV=$1
+
+cp -r ../../bbprop .
+
+# sleep 3
+
+if [ "$ENV" == "local" ]
+then
+    echo "Building local Docker image..."
+    docker build -t bbprop_cloud_run .
+elif [ "$ENV" == "remote" ]
+then
+    echo "Building Google Cloud image..."
+    gcloud builds submit --tag gcr.io/bbprop/bbprop_cloud_run
+else
+    echo "Use 'local' or 'remote' as script argument."
+fi
+
+rm -r bbprop
