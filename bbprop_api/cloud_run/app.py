8920f54cdae1137d8d14de123433b87d126f0fd6
Jeremy Morrison <jeremy.morrison36@gmail.com>
2021-02-19 18:17:50 -0800

    Add test routes and tests to diagnose timeout
    '/nba_game_logs' is timing out on Cloud Run only. Break out function calls into separate
    tests to find the problem.

--- bbprop_api/cloud_run/app.py
+++ bbprop_api/cloud_run/app.py
@@ -1,7 +1,11 @@
 from flask import Flask, jsonify
-import chromedriver_binary  # Adds chromedriver binary to path
+
+# Add chromedriver binary to path.
+import chromedriver_binary  # noqa: F401
 
 from bbprop.pinnacle import Pinnacle
+from bbprop.sportapi import NBA
+from middlewares import s3storage, write_nba_game_logs
 
 
 def create_app(
@@ -11,9 +15,48 @@ def create_app(
 
     @app.route("/selenium")
     def scrape():
-        pin = Pinnacle(*driver_args)
-        pg = pin.league()
+        with Pinnacle(*driver_args) as pin:
+            pg = pin.league()
 
         return jsonify(pg.related)
+
+    @app.route("/nba_game_logs")
+    def nightly_game_logs():
+        write_nba_game_logs(s3storage())
+        # Return status code 200.
+        return jsonify(success=True)
+
+    @app.route("/test-s3-conn")
+    def foo():
+        # return jsonify(bucket=os.getenv("S3_BUCKET"))
+        bucket = s3storage()
+
+        df = bucket.csv_to_dataframe("game-logs/Andre Drummond.csv")
+        return jsonify(df.to_json(orient="records"))
+
+    @app.route("/test-nba-conn")
+    def bar():
+        nba = NBA()
+        return jsonify(nba.player_by_name("Bradley Beal"))
+
+    @app.route("/test-s3-player-names")
+    def baz():
+        bucket = s3storage()
+        return jsonify(bucket.player_names())
+
+    @app.route("/test-nba-season-game-log-by-name")
+    def quux():
+        nba = NBA()
+        df = nba.season_game_log_by_name("Bradley Beal")
+        return jsonify(df.to_json(orient="records"))
+
+    @app.route("/test-s3-dataframe-to-csv")
+    def qux():
+        bucket = s3storage()
+
+        df = bucket.csv_to_dataframe("game-logs/Andre Drummond.csv")
+        bucket.dataframe_to_csv(df, "game-logs/Andre Drummond.csv")
+
+        return jsonify(success=True)
 
     return app
--- tests/test_cloud.py
+++ tests/test_cloud.py
@@ -13,7 +13,7 @@ def test_driver_local(localstorage):
 
 class TestLocal:
     # See Pytest Flask docs about client - it's from Flask
-    def test_local_flask_app(self, client, accept_json, app):
+    def test_selenium(self, client, accept_json, app):
         res = client.get("/selenium", headers=accept_json)
         assert len(res.json) > 0
 
@@ -30,23 +30,69 @@ class TestLocal:
     def test_get_nba_game_logs(self):
         pass
 
+    def test_test_s3_conn(self, client, accept_json, app):
+        res = client.get("/test-s3-conn", headers=accept_json)
+        # res.json not res.json() - pytest-flask, not requests.
+        assert len(res.json) > 0
+
 
 class TestDocker:
-    def test_docker_flask_app(self):
-        # Docker container must be running locally
-        res = requests.get("http://localhost:8080/selenium")
+    """Class for testing local Docker instance, and Docker instance deployed to Cloud
+    Run.
+    """
+
+    @pytest.mark.parametrize(
+        "api_url", [(os.getenv("LOCAL_API")), (os.getenv("GCLOUD_API"))]
+    )
+    def test_selenium(self, api_url):
+        res = requests.get(f"{api_url}/selenium")
         assert len(res.json()) > 0
 
-    def test_docker_flask_s3_conn(self):
-        res = requests.get("http://localhost:8080/test-s3-conn")
+    @pytest.mark.parametrize(
+        "api_url", [(os.getenv("LOCAL_API")), (os.getenv("GCLOUD_API"))]
+    )
+    def test_test_s3_conn(self, api_url):
+        res = requests.get(f"{api_url}/test-s3-conn")
         assert len(res.json()) > 0
 
-    def test_docker_flask_nba_game_logs(self, s3storage):
-
-        pass
-
-
-class TestGCloud:
-    def test_gcloud_flask_app(self):
-        res = requests.get(f"{os.getenv('GCLOUD_API')}/selenium")
+    @pytest.mark.parametrize(
+        "api_url", [(os.getenv("LOCAL_API")), (os.getenv("GCLOUD_API"))]
+    )
+    def test_test_nba_conn(self, api_url):
+        res = requests.get(f"{api_url}/test-nba-conn")
         assert len(res.json()) > 0
+
+    @pytest.mark.parametrize(
+        "api_url", [(os.getenv("LOCAL_API")), (os.getenv("GCLOUD_API"))]
+    )
+    def test_test_s3_player_names(self, api_url):
+        res = requests.get(f"{api_url}/test-s3-player-names")
+        assert len(res.json()) > 0
+
+    @pytest.mark.parametrize(
+        "api_url", [(os.getenv("LOCAL_API")), (os.getenv("GCLOUD_API"))]
+    )
+    def test_test_nba_season_game_log_by_name(self, api_url):
+        res = requests.get(f"{api_url}/test-nba-season-game-log-by-name")
+        assert len(res.json()) > 0
+
+    @pytest.mark.parametrize(
+        "api_url", [(os.getenv("LOCAL_API")), (os.getenv("GCLOUD_API"))]
+    )
+    def test_test_s3_dataframe_to_csv(self, api_url):
+        res = requests.get(f"{api_url}/test-s3-dataframe-to-csv")
+        assert len(res.json()) > 0
+
+    @pytest.mark.parametrize(
+        "api_url", [(os.getenv("LOCAL_API")), (os.getenv("GCLOUD_API"))]
+    )
+    def test_nba_game_logs(self, api_url, s3storage):
+        """Test nba_game_logs route that will be run nightly.
+
+        s3storage and storage object in container must point to the same bucket.
+        """
+        old_count = len(s3storage.player_names())
+        res = requests.get(f"{api_url}/nba_game_logs")
+        new_count = len(s3storage.player_names())
+        assert old_count == new_count
+        assert res.status_code == 200
