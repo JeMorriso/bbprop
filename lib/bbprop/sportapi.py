28d1764a530546a5384fd5d9eb6136bc1fcb7196
Jeremy Morrison <jeremy.morrison36@gmail.com>
2021-02-23 21:57:23 -0800

    Finish implementing BallDontLieAdapter

--- lib/bbprop/sportapi.py
+++ lib/bbprop/sportapi.py
@@ -124,6 +124,50 @@ class BallDontLieAdapter(BallDontLie):
     def _adapt_season_string(self, season):
         return season.split("-")[0]
 
+    def _clean_game_logs(self, game_logs):
+        def adapt_season_to_nba(season):
+            return f"{season}-{season % 2000 + 1}"
+
+        def count_double(game):
+            count = 0
+            for cat in ("ast", "blk", "stl", "pts", "reb"):
+                count += int(game[cat] > 9)
+            return count
+
+        cleaned = []
+        for g in game_logs:
+            date_ = g["game"]["date"].split("T")[0]
+            season = adapt_season_to_nba(g["game"]["season"])
+            doubles = count_double(g)
+            double_double = 1 if doubles > 1 else 0
+            triple_double = 1 if doubles > 2 else 0
+            pname = f"{g['player']['first_name']} {g['player']['last_name']}"
+
+            cleaned.append(
+                {
+                    "GAME_DATE": date_,
+                    "SEASON_YEAR": season,
+                    "PTS": g["pts"],
+                    "AST": g["ast"],
+                    "STL": g["stl"],
+                    "TOV": g["turnover"],
+                    "FG3M": g["fg3m"],
+                    "REB": g["reb"],
+                    "BLK": g["blk"],
+                    "DD2": double_double,
+                    "TD3": triple_double,
+                    "PLAYER_ID": g["player"]["id"],
+                    "PLAYER_NAME": pname,
+                    "PLAYER_FIRST_NAME": g["player"]["first_name"],
+                    "PLAYER_LAST_NAME": g["player"]["last_name"],
+                    "TEAM_ABBREVIATION": g["team"]["abbreviation"],
+                    "TEAM_ID": g["team"]["id"],
+                    "HOME_TEAM_ID": g["game"]["home_team_id"],
+                    "AWAY_TEAM_ID": g["game"]["visitor_team_id"],
+                }
+            )
+        return pd.DataFrame(cleaned)
+
     def season_game_log(self, player_id, season):
         """Get player's game logs for season, and convert into target format.
 
@@ -139,6 +183,7 @@ class BallDontLieAdapter(BallDontLie):
     def season_game_log_by_name(self, name, season):
         player = self.player_by_name(name)
         if player:
-            return self.season_game_log(player["id"], season)
+            glg = super().season_game_log(player["id"], season)
+            return self._clean_game_logs(glg)
         else:
             return pd.DataFrame()
