79a0a087cba34045f74e15145f4d8e6a475769a2
Jeremy Morrison <jeremy.morrison36@gmail.com>
2021-02-25 21:37:25 -0800

    Implement and test '/players' route

--- apis/chalice/app.py
+++ apis/chalice/app.py
@@ -1,19 +1,30 @@
+import os
+
 from chalice import Chalice
 import boto3
+import requests
 
-from chalicelib.storage import S3Storage
+from chalicelib.storage import S3Storage, BallDontLieStorage
 
 app = Chalice(app_name="bbprop-api")
 
 
 @app.route("/nextjs")
-def get_static_data():
-    """Return data from most recent bet values .csv.
+def scrape_and_deploy():
+    """Trigger Cloud Run, Vercel deploy hook, and store bet values .csv.
 
     Lambda function must have permission to access S3 bucket.
     """
-    store = S3Storage(boto3.session.Session(), bucket="bbprop")
-    store.csv_to_dataframe("bets/2021-02-25T21:15:38.csv")
-    pass
+    #     res = requests.get(f'{os.getenv("GCLOUD_API")}/selenium')
+    # store = S3Storage(boto3.session.Session(), bucket="bbprop")
+    # store.csv_to_dataframe("bets/2021-02-25T21:15:38.csv")
 
-    return {"hello": "world"}
+    return {"hello": os.getenv("GCLOUD_API")}
+
+
+@app.route("/players")
+def players():
+    """Send BallDontLie players to Cloud Run."""
+    store = BallDontLieStorage(S3Storage(boto3.session.Session(), bucket="bbprop"))
+    players = store.players()
+    return players
--- tests/test_chalice.py
+++ tests/test_chalice.py
@@ -1,11 +1,24 @@
 import os
 
 from chalice.test import Client
+from chalice import Chalice
 import requests
+from flask import jsonify
 
-# import pytest
-
+from chalicelib.storage import LocalStorage, BallDontLieStorage
 from apis.chalice.app import app
+
+
+testapp = Chalice(app_name="test-bbprop-api")
+
+
+@testapp.route("/players")
+def players():
+    """Send BallDontLie players to Cloud Run."""
+    store = BallDontLieStorage(LocalStorage("tests/json"))
+    players = store.players()
+    return players
+    # return jsonify(players)
 
 
 def test_nextjs_local():
@@ -18,3 +31,14 @@ def test_nextjs_remote():
     res = requests.get(f'{os.getenv("LAMBDA_API")}/nextjs')
     assert res.json() == {"hello": "world"}
     pass
+
+
+def test_testapp_player():
+    with Client(testapp) as client:
+        res = client.http.get("/players")
+        assert len(res.json_body) > 0
+
+
+def test_app_player():
+    res = requests.get(f'{os.getenv("LAMBDA_API")}/players')
+    assert len(res.json()) > 0
