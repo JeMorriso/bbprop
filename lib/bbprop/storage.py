e7b44d6fe5ebb975026e23430d3707d06fd37d26
Jeremy Morrison <jeremy.morrison36@gmail.com>
2021-04-06 18:48:00 -0700

    Add LeagueStorage
    Replace BallDontLieStorage with league-flexible class

--- lib/bbprop/storage.py
+++ lib/bbprop/storage.py
@@ -1,34 +1,13 @@
-from abc import ABC, abstractmethod
 from io import StringIO
 from pathlib import Path
-import re
 import json
 
 import pandas as pd
 
 
-class Storage(ABC):
-    def __init__(self):
-        pass
-
-    @abstractmethod
-    def dataframe_to_csv(self, df, path):
-        pass
-
-    @abstractmethod
-    def csv_to_dataframe(self, path):
-        pass
-
-
-class LocalStorage(Storage):
+class LocalStorage:
     def __init__(self, dir):
-        # add trailing slash to make path compatible with local or s3
-        self.dir = f"{str((Path() / Path(dir).resolve()))}/"
-        self.game_log_dir = f"{self.dir}game-logs"
-        self.bets_dir = f"{self.dir}bets"
-
-    def root_dir(self):
-        return self.dir
+        self.dir = f"{str((Path() / Path(dir).resolve()))}"
 
     def dataframe_to_csv(self, df, path):
         df.to_csv(path, header=True, index=False)
@@ -36,27 +15,28 @@ class LocalStorage(Storage):
     def csv_to_dataframe(self, path):
         return pd.read_csv(path)
 
-    def player_names(self):
-        csvs = [p for p in Path(self.game_log_dir).iterdir() if p.is_file()]
-        return [csv.name.replace(".csv", "") for csv in csvs]
-
     def load_json(self, path):
         with open(path, "r") as f:
             return json.load(f)
 
+    def generate_path(self, *args):
+        clean_args = [a for a in args if a]
+        return "/".join(clean_args)
 
-class S3Storage(Storage):
-    def __init__(self, session, bucket):
+
+class S3Storage:
+    """Storage class interfacing with AWS S3 bucket.
+
+    Root directory should not have slash at beginning.
+    """
+
+    def __init__(self, session, bucket, dir=""):
         # boto3 session from user or lambda role
         self.client = session.client("s3")
         self.resource = session.resource("s3")
         self.bucket = self.resource.Bucket(bucket)
-        self.game_log_dir = "game-logs"
-        self.bets_dir = "bets"
-        self.latest_dir = f"{self.bets_dir}/latest"
 
-    def root_dir(self):
-        return ""
+        self.dir = dir
 
     def dataframe_to_csv(self, df, path):
         # don't export to file, return csv string
@@ -71,16 +51,6 @@ class S3Storage(Storage):
         csv_str = obj["Body"].read().decode("UTF-8")
 
         return pd.read_csv(StringIO(csv_str))
-
-    def player_names(self):
-        prefix = f"{self.game_log_dir}/"
-        objects = self.client.list_objects_v2(Bucket=self.bucket.name, Prefix=prefix)
-        keys = [f["Key"] for f in objects["Contents"]]
-        # Remove the matching directory, Prefix doesn't support regex.
-        paths = [k for k in keys if re.match(fr"{prefix}.+", k)]
-        files = [re.sub(fr"^{prefix}", "", f) for f in paths]
-        players = [re.sub(r"\.csv$", "", f) for f in files]
-        return players
 
     def find_files(self, path):
         """Return paths of files with matching prefix."""
@@ -121,18 +91,32 @@ class S3Storage(Storage):
         obj_str = obj["Body"].read().decode()
         return json.loads(obj_str)
 
+    def generate_path(self, *args):
+        clean_args = [a for a in args if a]
+        return "/".join(clean_args)
 
-class BallDontLieStorage:
-    """Compose LocalStorage / S3Storage, add BallDontLie specific configuration."""
 
-    def __init__(self, store):
+class LeagueStorage:
+    """Compose LocalStorage / S3Storage, add League specific storage configuration.
+
+    Better to use composition here instead of trying to inherit from both.
+
+    Attributes:
+        dir: League root directory.
+        players_dir: Directory where players.json is found.
+        bets_dir: Directory where archived bets csv files can be found.
+        latest_dir: Directory holding the latest bets csv to be used by NextJS.
+    """
+
+    def __init__(self, store, dir):
         self.store = store
-        self.dir = "players"
-
-    def bets_dir(self):
-        """Return path to bets directory."""
-        return self.store.bets_dir
+        self.dir = dir
+        self.players_dir = "players"
+        self.bets_dir = "bets"
+        self.latest_dir = "latest"
 
     def players(self):
-        path = f"{self.store.root_dir()}{self.dir}/balldontlie_players.json"
+        path = self.store.generate_path(
+            self.store.dir, self.dir, self.players_dir, "players.json"
+        )
         return self.store.load_json(path)
